9ab15de949df2f3f1681489cafd52d5605e45799
ByeongUk-An <quddnr005@naver.com>
2020-10-25 20:53:46 +0900

    login ui function

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+const Footer = (props) => (
+  <footer className={styles.footer}>
+    <p className={styles.title}>Follow your Dream</p>
+  </footer>
+);
+
+export default Footer;
